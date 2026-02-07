bb9a94102c77a6e255add141cb89708d0864ee6e
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-07 17:08:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+// Serve static files from public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Login page
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Dashboard page
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
