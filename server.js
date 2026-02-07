693a04dc2460d337d5600142201b545ff20365b2
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-07 15:30:31 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Login page
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
