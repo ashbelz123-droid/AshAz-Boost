ccf96317ab54051c04ad3889dfc0d8b2d984df5c
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-07 14:39:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files (CSS, JS, images)
+// Serve static files like CSS, JS, images
 app.use(express.static(path.join(__dirname)));
 
-// Route to serve index.html
+// Route for home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route for login page
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
