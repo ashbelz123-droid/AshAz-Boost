8b71b35d14c89febe416d96d3b35f7cd8c754800
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-08 09:46:05 +0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.model(
-  "Order",
-  new mongoose.Schema({
-    service: Number,
-    link: String,
-    quantity: Number,
-    price: Number,
-    providerOrderId: Number,
-    status: String,
-    refunded: { type: Boolean, default: false }
-  })
-);
+module.exports = mongoose.model("Order", new mongoose.Schema({
+  service: String,
+  link: String,
+  quantity: Number,
+  price: Number,
+  providerOrderId: Number,
+  status: String,
+  refunded: { type: Boolean, default: false }
+}));
