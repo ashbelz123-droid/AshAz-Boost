6eca02c862b9d75e586707723d186cc863e43faf
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-08 07:47:42 +0300

    Create Order.js

---
+++ models/Order.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model(
+  "Order",
+  new mongoose.Schema({
+    service: Number,
+    link: String,
+    quantity: Number,
+    price: Number,
+    providerOrderId: Number,
+    status: String,
+    refunded: { type: Boolean, default: false }
+  })
+);
